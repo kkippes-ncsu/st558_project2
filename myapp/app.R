6b6a31168a80ae1907987210f45a8d7c232a7516
Kayla Kippes <kkippes@ncsu.edu>
2025-07-06 15:46:47 -0400

    about tab

--- myapp/app.R
+++ myapp/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(bslib)
 library(httr)
 library(tidyverse)
+library(ggplot2)
 
 #gives detailed view of breweries based on certain parameters
 #only returns max 200 though
@@ -66,8 +67,28 @@ brewery_random <- function(n, country = NULL, state = NULL, city = NULL, type = 
 
 
 # Define UI ----
-ui <- page_sidebar(
-  title = "Brewlytics - Shiny App for Breweries"
+ui <- fluidPage(
+  titlePanel('Brewlytics - Shiny App for Breweries'),
+  tabsetPanel(
+    tabPanel('About',
+             h2('Purpose'),
+             p('The purpose of this app is to fulfill the requirements of Project 2 in ST 558. This app will explore different breweries in the', em('Open Brewery Database.'),
+               'This app will allow its users to filter the brewery data set, download chosen subsets of the data, and view numerical or graphical summaries dependent on the selected parameters.'),
+             h2('The Data'),
+             p('Ranging all across the world, information like brewery id, name, location, type, phone number, and website can be found in this database. Location information is
+               presented from address all the way down to coordinates. The source of the data can be found below:'), a('Open Brewery DB', href = "https://www.openbrewerydb.org/"),
+             h2('Tabs'),
+             h4('About'),
+             p('The purpose of the about tab is to give a general overview of the app, the data set and where to find the data.'),
+             h4('Data Download'),
+             p('The purpose of the data download tab is to allow the user to make changes to the API query functions, return and display the data,
+               subset this data, and then download and save the data.'),
+             h4('Data Exploration'),
+             p('The purpose of the data exploration tab is to allow the user to choose different varaiables and combination of variables to view numerical and graphical summaries.'),
+             tags$img(height = 300, width = 400, src = 'https://bundobust.com/wp-content/uploads/2023/05/2021-09-14_BundobustBrewery-114-scaled.jpg')),
+    tabPanel('Data Download'),
+    tabPanel('Data Exploration')
+  )
 )
 
 # Define server logic ----
